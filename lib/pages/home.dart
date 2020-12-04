a27d8cab562c1f8f0c8de0e1f7a180f7ce014bcf
sm472 <>
2020-12-04 13:18:24 +1300

    Background images for day and night

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,39 +14,59 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
+
+    //set background
+    String bgimage = data['isdaytime'] ? 'day.png':'night.png';
+    Color bgColor = data['isdaytime'] ? Colors.blue : Colors.indigo[700];
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(child:
-      Padding(
-        padding: const EdgeInsets.fromLTRB(0,120,0,0),
-        child: Column(
+      Container(
+        decoration: BoxDecoration(
+          image:DecorationImage(
+            image: AssetImage('assets/$bgimage'),
+            fit:BoxFit.cover
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0,120,0,0),
+          child: Column(
 
-          children: [
-            FlatButton.icon(onPressed: (){Navigator.pushNamed((context), '/location');},
-            icon : Icon(Icons.edit_location),
-            label: Text('Edit Location'),),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  data['location'],
-                  style: TextStyle(
-                    fontSize: 28,
-                    letterSpacing: 2
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 20),
-            Text(data['time'],
-              style: TextStyle(
-                fontSize: 66
-              ),)
-
-          ],
+            children: [
+              FlatButton.icon(onPressed: (){Navigator.pushNamed((context), '/location');},
+              icon : Icon(Icons.edit_location,
+              color: Colors.grey[300],),
+              label: Text(
+                'Edit Location',
+                style:
+                TextStyle(color: Colors.grey[300]),
+              ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      letterSpacing: 2
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(data['time'],
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 66
+                ),)
+            ],
+          ),
         ),
       )),
-
     );
   }
 }
